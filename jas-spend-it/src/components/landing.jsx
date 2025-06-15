90c5399971a5adb26bbd9e7f863a04eed16169ff
jjelybean <angeljasmine743@gmail.com>
2025-06-15 15:15:21 +0800

    visualized front end for landing. not done

--- jas-spend-it/src/components/landing.jsx
+++ jas-spend-it/src/components/landing.jsx
@@ -51,7 +51,7 @@ function Landing() {
 
     {/*next section*/}
 
-    <section className='body'>
+    <section className='body py-3'>
         <motion.div
             className="container text-center p-5 bg-white rounded shadow"
             initial={{ opacity: 0, y: 50 }}
@@ -63,7 +63,7 @@ function Landing() {
             <p className="lead">
              <strong>JasSpendIt</strong> is a budgeting website designed to help you track your spending, 
              set savings goals, and stay on top of your finances — all in one easy-to-use place. Whether you're saving for something big, managing monthly 
-             expenses, or just want to be smarter with your money, we’ve got your back! 💼💖
+             expenses, or just want to be smarter with your money, we got your back! 💼💖
             </p>
         </motion.div>
     </section>
@@ -112,6 +112,82 @@ function Landing() {
    
 
     </section>
+
+    {/* ABOUT US SECTION */}
+
+    <section className="body py-3">
+        <div className="container section-title" data-aos="fade-up">
+            <h2>About Us</h2>
+            <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        </div>
+      
+    <div className="container">
+        <div className="row gy-4">
+          <div className="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i className="bi bi-check2-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
+              <li><i className="bi bi-check2-circle"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
+              <li><i className="bi bi-check2-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo</span></li>
+            </ul>
+          </div>
+
+          <div className="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+            <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <a href="#" className="read-more"><span>Read More</span><i className="bi bi-arrow-right"></i></a>
+          </div> {/* col */}
+        </div> {/* row */}
+    </div>  {/* container */}
+    </section>
+
+    {/* footer section here */}
+
+    <footer className="footer light-background">
+
+    <div className="container footer-top">
+      <div className="row gy-4">
+        <div className="col col-lg-6 ">
+          <a href="index.html" className="logo d-flex align-items-center">
+            <span className="sitename">OnePage</span>
+          </a>
+          <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+          <div className="social-links d-flex mt-4">
+            <a href=""><i className="bi bi-twitter-x"></i></a>
+            <a href=""><i className="bi bi-facebook"></i></a>
+            <a href=""><i className="bi bi-instagram"></i></a>
+            <a href=""><i className="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div className="col-lg-3 ">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Terms of service</a></li>
+            <li><a href="#">Privacy policy</a></li>
+          </ul>
+        </div>
+
+        <div className="col-lg-3">
+          <h4>Contact Us</h4>
+          <p>A108 Adam Street</p>
+          <p>New York, NY 535022</p>
+          <p>United States</p>
+          <p className="mt-4"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
+          <p><strong>Email:</strong> <span>info@example.com</span></p>
+        </div>
+
+      </div>
+    </div>
+
+
+
+  </footer>
     </>
   );
 }
